06e7493446db38fdd02fa2dccb88e5e555c6c6bc
Anurag Bhandari <anurag.bhd@gmail.com>
2020-04-09 08:52:27 +0530

    Polish LoginScreen a bit

--- lib/screens/login/email_password_form.dart
+++ lib/screens/login/email_password_form.dart
@@ -1,5 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:foodieapp/constants.dart';
+import 'package:foodieapp/widgets/page_action_button.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -12,67 +15,87 @@ class EmailPasswordFormState extends State<EmailPasswordForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  bool _success;
-  String _userEmail;
+
+  String _errorMsg = '';
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        overflow: Overflow.visible,
         children: <Widget>[
           Container(
-            child: const Text('Test sign in with email and password'),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(labelText: 'Email'),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _passwordController,
-            decoration: const InputDecoration(labelText: 'Password'),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            alignment: Alignment.center,
-            child: RaisedButton(
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  _signInWithEmailAndPassword();
-                }
-              },
-              child: const Text('Submit'),
+            padding: kPaddingAll,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: kContBorderRadiusSm,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Email cannot be blank';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Password cannot be blank';
+                      }
+                      return null;
+                    },
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Text(
+                      this._errorMsg,
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              _success == null
-                  ? ''
-                  : (_success
-                      ? 'Successfully signed in ' + _userEmail
-                      : 'Sign in failed'),
-              style: TextStyle(color: Colors.red),
+          Positioned(
+            bottom: -25.0,
+            child: Container(
+              width: MediaQuery.of(context).size.width - kPaddingUnits * 2,
+              padding: EdgeInsets.symmetric(horizontal: kPaddingUnits),
+              child: PageActionButton(
+                text: 'Come on in',
+                onPressed: this._submitForm,
+              ),
             ),
-          )
-        ],
-      ),
-    );
+          ),
+        ]);
+  }
+
+  void _submitForm() async {
+    if (_formKey.currentState.validate()) {
+      try {
+        await _auth.signInWithEmailAndPassword(
+          email: this._emailController.text,
+          password: this._passwordController.text,
+        );
+      } on PlatformException catch (e) {
+        setState(() {
+          this._errorMsg = e.message;
+        });
+      }
+    }
   }
 
   @override
@@ -80,22 +103,5 @@ class EmailPasswordFormState extends State<EmailPasswordForm> {
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
-  }
-
-  // Example code of how to sign in with email and password.
-  void _signInWithEmailAndPassword() async {
-    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
-      email: _emailController.text,
-      password: _passwordController.text,
-    ))
-        .user;
-    if (user != null) {
-      setState(() {
-        _success = true;
-        _userEmail = user.email;
-      });
-    } else {
-      _success = false;
-    }
   }
 }
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:foodieapp/constants.dart';
 import 'package:foodieapp/screens/login/email_password_form.dart';
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class LoginScreen extends StatelessWidget {
   static const id = 'login';
@@ -10,43 +8,18 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign in'),
-        actions: <Widget>[
-          Builder(builder: (BuildContext context) {
-            return FlatButton(
-              child: const Text('Sign out'),
-              textColor: Theme.of(context).buttonColor,
-              onPressed: () async {
-                final FirebaseUser user = await _auth.currentUser();
-                if (user == null) {
-                  Scaffold.of(context).showSnackBar(const SnackBar(
-                    content: Text('No one has signed in.'),
-                  ));
-                  return;
-                }
-                _signOut();
-                final String uid = user.uid;
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text(uid + ' has successfully signed out.'),
-                ));
-              },
-            );
-          })
-        ],
-      ),
-      body: Builder(builder: (BuildContext context) {
-        return ListView(
-          scrollDirection: Axis.vertical,
+      body: Container(
+        padding: kPaddingHorizontal,
+        decoration: BoxDecoration(
+          color: kColorBluegrey,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             EmailPasswordForm(),
           ],
-        );
-      }),
+        ),
+      ),
     );
-  }
-
-  void _signOut() async {
-    await _auth.signOut();
   }
 }
