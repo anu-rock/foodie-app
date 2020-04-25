0f0bdc563c1b12376037834e4bb2ab98d9f58668
Anurag Bhandari <anurag.bhd@gmail.com>
2020-04-25 13:37:26 +0530

    Fix data on RecipeDirectionsScreen

--- lib/screens/recipe_directions/recipe_directions_screen.dart
+++ lib/screens/recipe_directions/recipe_directions_screen.dart
@@ -55,7 +55,7 @@ class RecipeDirectionsScreen extends StatelessWidget {
                         SizedBox(
                           height: 20.0,
                         ),
-                        ...recipe.ingredients
+                        ...recipe.instructions
                             .asMap()
                             .map((idx, direction) => MapEntry(
                                   idx,
